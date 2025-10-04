065f86d0fcca065af1fa64628d3721c88a9f2354
Akula Jahnavi <138507788+Akula-Jahnavi@users.noreply.github.com>
2025-10-04 11:05:36 +0530

    Assignment 4 UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/pic2.png'
-import viteLogo from '/pic1.png'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [likes, setLikes] = useState(0);
+
+  // Direct Updates: setLikes(likes + 1)
+  const handleDirectUpdate = () => {
+    setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+  };
+
+  // Functional Updates: setLikes(prevLikes => prevLikes + 1)
+  const handleFunctionalUpdate = () => {
+    setLikes((prevLikes) => prevLikes + 1);
+    setLikes((prevLikes) => prevLikes + 1);
+    setLikes((prevLikes) => prevLikes + 1);
+    setLikes((prevLikes) => prevLikes + 1);
+
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Likes: {likes}</h2>
+      <button onClick={handleDirectUpdate}>Button 1 (Direct Updates)</button>
+      <br /><br />
+      <button onClick={handleFunctionalUpdate}>Button 2 (Functional Updates)</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
